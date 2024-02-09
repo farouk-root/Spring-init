94cdb9d4dbda8ea5675900c541eb4232ed80aa35
farouk-root <farouk.chalghoumi031@gmail.com>
2024-02-09 12:56:29 +0100

    init ( configured data jpa & hibernate )

---
+++ src/main/java/tn/esprit/intispring/IntiSpringApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.intispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntiSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/tn/esprit/intispring/cours/cours.java
@@ -0,0 +1,4 @@
+package tn.esprit.intispring.cours;
+
+public class cours {
+}
